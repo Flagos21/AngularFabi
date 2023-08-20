e5e43804d198f3d477942377e3e62e4e2eda546a
Jose Sepulveda <josealejandrosepulvedapino@gmail.com>
2023-08-20 16:19:39 -0400

    feat: component prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponentComponent } from './components/prueba-component/prueba-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponentComponent
   ],
   imports: [
     BrowserModule,
